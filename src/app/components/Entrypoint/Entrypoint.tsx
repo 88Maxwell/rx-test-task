369389aa75ef17b6e66bf313c8ed638a321d97f3
Soltyk Maksym <maxwell88soltyk@gmail.com>
2024-07-17 19:13:35 +0300

    WIP: add router and tailwind

---
+++ src/app/components/Entrypoint/Entrypoint.tsx
@@ -0,0 +1,10 @@
+import { Theme } from "@radix-ui/themes";
+import { App } from "../App";
+
+export function Entrypoint() {
+  return (
+    <Theme>
+      <App />
+    </Theme>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./input.css";
+import "@radix-ui/themes/styles.css";
+import { Entrypoint } from "./app/components/Entrypoint";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Entrypoint />
+  </React.StrictMode>
+);
